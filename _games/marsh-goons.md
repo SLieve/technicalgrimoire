570d04eb568d2a1537b4f59c1adccf4001456af4
David Schirduan <davids@ceterus.com>
2020-02-07 21:42:41 -0500

    added marsh goons generator

--- _games/marsh-goons.md
+++ _games/marsh-goons.md
@@ -17,7 +17,34 @@ Marsh Goons is about tromping through a bizarre marsh, encountering/surviving th
 
 Join [the Mailing List](https://gumroad.com/technicalgrimoire/follow) to get updates about the upcomign zine!
 
-**Marsh Goons will include:**
+<div class="row centerButtons">
+  <div class="col-8">
+    <button id="CharButton" class="btn bonemarshes-btn" onclick="generate()">
+      <h3>Generate Character</h3>
+    </button>  
+    </div>
+</div>
+
+<div class="container bonemarshesCard" id="charCard">
+  <div class="row">
+		<div class="col-md-6 col-12"><h2 id="charName">Testing</h2></div>
+		<div class="col-md-3 col-6"><h3 id="charHP">23</h3></div>
+		<div class="col-md-3 col-6"><h3>Lvl: 1</h3></div>
+  </div>
+  <hr>
+  <div class="row">
+		<div class="col-md col-10"><h3 id="charPOW"></h3></div>
+		<div class="col-md col-10"><h3 id="charINS"></h3></div>
+		<div class="col-md col-10"><h3 id="charKNO"></h3></div>
+	</div>
+  <hr>
+  <h2 id="charEquip">Starting Equipment</h2>
+  <p>Choose <strong>three</strong> of the items below to start. Unless otherwise noted, each item takes up one slot.</p>
+  <p id="charItems"></p>
+  <h3 style="text-align: center;">TAKE A SCREENSHOT SO YOU DON'T LOSE YOUR CHARACTER</h3>
+</div>
+
+### Marsh Goons will include:
 
  - Artwork by [Jess Comstock](https://www.jessidraws.art/)
  - Hexkit maps by [Cecil Howe](https://cone.itch.io/) and [Steven Colling](https://stevencolling.itch.io/isle-of-lore-2-hex-tiles-regular)
@@ -30,3 +57,5 @@ Join [the Mailing List](https://gumroad.com/technicalgrimoire/follow) to get upd
  - And more!
 
 ![MG_cover.png.png](/images/posts/MG_cover.png)
+
+<script async src="/_pages/mg_generator.js" charset="utf-8"></script>
---
+++ _pages/mg_generator.js
@@ -0,0 +1,96 @@
+/** TODO: 
+ * Add all the images
+ * Expand the text descriptions of each creature
+ */
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    mg = JSON.parse(this.responseText);
+  }
+};
+xmlhttp.open("GET", "/_pages/mg_generator.json", true);
+xmlhttp.send();
+
+
+
+function rollHP() {
+  var die1 = Math.floor(Math.random() * 4) + 1;
+  var die2 = Math.floor(Math.random() * 4) + 1;
+  var die3 = Math.floor(Math.random() * 4) + 1;
+  return die1 + die2 + die3 + 4;
+}
+
+function rollStats() {
+  var points = 3;
+var POW = 0;
+var INS = 0;
+var KNO = 0;
+
+  while (points > 0) {
+    random = Math.random();
+
+    if (random <= .33 && POW < 2) {
+      POW++;
+      points--;
+    } else if (random <= .66 && INS < 2) {
+      INS++;
+      points--;
+    } else if (random <= 1 && KNO < 2) {
+      KNO++;
+      points--;
+    }
+  }
+
+      document.getElementById("charPOW").innerText = "Power: " + POW;
+      document.getElementById("charINS").innerText = "Instinct: " + INS;
+      document.getElementById("charKNO").innerText = "Knowledge: " + KNO;
+}
+
+  function selectRandom(jsonList) {
+    result = jsonList[Math.floor(Math.random() * jsonList.length)];
+    if (Array.isArray(result)) {
+      result = selectRandom(result);
+    }
+    return result;
+  }
+
+  function generate() {
+
+    document.getElementById("charName").innerText = "Name: " + selectRandom(mg.Names);
+
+    /* ======= STATS ======= */
+    rollStats();
+
+    document.getElementById("charHP").innerText = "Hit Points: " + rollHP();
+
+    /* ======= EQUIPMENT ======= */
+    //Show all items: melee, ranged, armor, rations, and 3 random items
+
+    document.getElementById("charItems").innerHTML =
+      '<div class="row">' +
+      '<div class="col-md-4 col-6"> • <strong>Weapon:</strong> ' +
+      selectRandom(mg.MeleeWeapons) +
+      " </div>" +
+      '<div class="col-md-4 col-6"> • <strong>Weapon:</strong> ' +
+      selectRandom(mg.RangedWeapons) +
+      " </div>" +
+      '<div class="col-md-4 col-6"> • <strong>Armor:</strong> ' +
+      selectRandom(mg.Armor) +
+      "</div>" +
+      '<div class="col-md-4 col-6"> • Rations (10 uses)</div>' +
+      '<div class="col-md-4 col-6"> • Torches (10 uses)</div>' +
+      '<div class="col-md-4 col-6"> • Ten-foot pole</div>' +
+      '<div class="col-md-4 col-6"> • Coin (100)</div>' +
+      '<div class="col-md-4 col-6"> • Flint and Steel</div>' +
+      '<div class="col-md-4 col-6"> • Pen and Paper</div>' +
+      '<div class="col-md-4 col-6"> • Skin of fresh water</div>' +
+      '<div class="col-md-4 col-6"> • Bottle of fine wine (3 uses)</div>' +
+      '<div class="col-md-4 col-6"> • Fine instrument of your choice</div>' +
+      '<div class="col-md-4 col-6"> • Faithful Pet (name, describe) <i>If discarded, is lost forever</i></div>' +
+      '<div class="col-md-4 col-6"> • Fancy clothes <i>If discarded, is lost forever</i></div>' +
+      '<div class="col-md-4 col-6"> • Spell Rune <i>(of a random spell)</i></div></div>';
+
+  document.getElementById("charCard").style = "display:block";
+    
+  }
