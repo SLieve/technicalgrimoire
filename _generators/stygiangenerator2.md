0ca2731c05275dc526e243f69445cf09a0265551
David Schirduan <davids@ceterus.com>
2020-08-12 22:13:29 -0400

    groundwork for Stygian

---
+++ _generators/stygiangenerator2.md
@@ -0,0 +1,59 @@
+---
+date: 2019-08-01
+layout: full-page
+title: Stygian Library Generator 2
+permalink: stygiangenerator2
+published: false
+image: /images/stygiangenerator.png
+description: >
+  A mobile-friendly generator for the Stygian Library.
+---
+
+<div class="stygian-cards">
+  <div class="stygian-text">
+    <div id="roomContent">
+      <h3 id="roomName">Welcome</h3>
+      <p>Books are condensed collections of knowledge, and knowledge is power. As any physicist will tell you, power is a function of energy, and energy and mass are interchangeable, and enough mass warps space time.</p>
+      <p>Knowledge is power. Power corrupts. The more power, the more it corrupts. Sufficient knowledge twists the world around it into strange shapes. Put enough books in one place, and they distort the world. Space bends in on itself, forming a sort of wormhole, linking the library to other libraries likewise afflicted. The space between is a sort of pocket realm, budded off from reality, maintained by the sheer power of books.</p>
+      <p>After you've bought a copy of the book, you can use the buttons to generate locations and events. <a href="https://www.drivethrurpg.com/product/257113/The-Stygian-Library">Buy it here.</a></p>
+    </div>
+    <div id="detailContent">
+    </div>
+    <div id="encounterContent">
+    </div>
+  </div>
+  <div class="stygian-log">
+    <div class="stygian-buttons">
+        <button id="backButton" class="stygian-button" type="button" onclick="goBack()" style="display:none;">Go Back</button>
+        <button class="stygian-button" type="button" onclick="goDeeper()">Go Deeper</button>
+        <button class="stygian-button" type="button"><span style="color:cornflowerblue;">Visitor</span> Event</button>
+        <button class="stygian-button" type="button"><span style="color:crimson;">Intruder</span> Event</button>
+    </div>
+    <hr class="stygian-hr-dark">
+    <div>
+      <h3>
+        Location Log
+      </h3>
+      <div id="logContent">
+        <ul>
+          <li>0: Entrance Foyer - Candles</li>
+          <li>1: Help Desk - Beehives</li>
+          <li>2: Boiler Room - Turning Gears</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--Necessary for allowing the sticky buttons and background changes-->
+<style>
+  body {
+    background-color: #313131;
+    color: #F5F5F5;
+  }
+  hy-push-state, hy-drawer {
+  overflow: unset;
+  }
+</style>
+
+<script async src="/assets/generator_resources/stygiangenerator.js" language="javascript" type="text/javascript"></script>
---
+++ assets/generator_resources/stygiangenerator.js
@@ -0,0 +1,96 @@
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    stygian = JSON.parse(this.responseText);
+  }
+};
+
+xmlhttp.open("GET", "/assets/generator_resources/stygian.json", true);
+xmlhttp.send();
+
+var currentLayer = 0;
+var visitor = true;
+var hrHTML = "<hr class=\"stygian-hr\">";
+
+//A list of number sets tracking the previous rooms and details. 
+//used when backtracking: [nextRoomNum, nextDetailNum, logDescr]
+var locationLog = [];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function goDeeper(){
+currentLayer = currentLayer + 1;
+
+nextRoomNum = getRandomInt(currentLayer, currentLayer+20);
+nextDetailNum = getRandomInt(currentLayer, currentLayer+20);
+
+//If above 35, then re-roll
+if (nextRoomNum >= 34)
+nextRoomNum = Math.floor(Math.random() * 20) + Math.floor(Math.random() * 12) + 1 + 2;
+
+//max for details is 35
+if (nextDetailNum > 34)
+nextDetailNum = 34;
+
+//add this to the log
+locationLog.push([nextRoomNum, nextDetailNum]);
+
+//build the room text
+document.getElementById("roomContent").innerHTML = "<h3>" + stygian.locations[nextRoomNum].title + "</h3>" + "<p>" + stygian.locations[nextRoomNum].description + "</p>" + hrHTML;
+
+//build the detail text
+document.getElementById("detailContent").innerHTML = "<h3>" + stygian.details[nextDetailNum].title + "</h3>" + "<p>" + stygian.details[nextDetailNum].description + "</p>" + hrHTML;
+
+updateLog();
+}
+
+function goBack(){
+  currentLayer = currentLayer - 1;
+
+  if (currentLayer <= 0)
+  currentLayer = 0;
+
+  //remove current location
+  locationLog.pop();
+
+  //Grab the previous one
+  backLocation = locationLog[locationLog.length - 1];
+  backRoom = backLocation[0];
+  backDetail = backLocation[1];
+
+//build the room text
+document.getElementById("roomContent").innerHTML = "<h3>" + stygian.locations[backRoom].title + "</h3>" + "<p>" + stygian.locations[backRoom].description + "</p>" + hrHTML;
+
+//build the detail text
+document.getElementById("detailContent").innerHTML = "<h3>" + stygian.details[backDetail].title + "</h3>" + "<p>" + stygian.details[backDetail].description + "</p>" + hrHTML;
+
+updateLog();
+
+}
+
+function updateLog(){
+  if (currentLayer <= 0){
+    document.getElementById("backButton").style = "display:none;";
+    currentLayer = 0;
+  }
+  else {
+    document.getElementById("backButton").style = "";
+  }
+
+  logHTML = "<ul>";
+  lvlCounter = 0;
+
+  for (const location of locationLog) {
+    lvlCounter = lvlCounter + 1;
+    logRoom = location[0];
+    logDetail = location[1];
+    logHTML = logHTML + "<li>Level " + lvlCounter + ": " + stygian.locations[logRoom].title + " - " + stygian.details[logDetail].title + "</li>";
+  }
+
+  document.getElementById("logContent").innerHTML = logHTML + "</ul>";
+}
