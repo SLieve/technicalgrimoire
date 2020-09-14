c913eb59c7c191afaefbed20b23f84948cf43bdf
David Schirduan <davids@ceterus.com>
2020-09-13 21:42:43 -0400

    fixed cleanup and starting new

--- _generators/tensiongame.md
+++ _generators/tensiongame.md
@@ -9,21 +9,18 @@ description: >
   A hand-eye coordination game similar to Jenga.
 ---
 
-My intention is to make a simple game that has similar hand-eye coordination as pulling Jenga blocks. 
-
-The more blobs you shoot, the more difficult the game gets. If you miss, Game Over! 
-
-_If you have feedback on tweaking the difficult or settings please let me know!_
+A mini-game that requires similar hand-eye coordination as pulling Jenga blocks. The more blobs you shoot, the more difficult things get. If you miss, Game Over! 
 
 <div id="jenga-buttons" class="row" style="justify-content: space-around !important;margin-bottom:30px;">
+  <!-- throwBalls(gameName, numBalls, ballSize, ballSpeed) -->
   <div class="col-md-4 col noPadding">
-  <a class="btn btn-black" style="background-color:#922b21;" onclick="window.throwBalls('dread', 6, 60, 6)"><h3 class="tightSpacing">Dread</h3>Default Settings</a></div>
+  <a class="btn btn-black" style="background-color:#922b21;" onclick="window.newGame('dread', 6, 60, 3)"><h3 class="tightSpacing">Dread</h3>Default Settings</a></div>
 
   <div class="col-md-4 col noPadding">
-  <a class="btn btn-black" style="background-color:#6c3483;" onclick="window.throwBalls('star', 3, 80, 10)"><h3 class="tightSpacing">StarCrossed</h3>Large, fast blobs</a></div>
+  <a class="btn btn-black" style="background-color:#6c3483;" onclick="window.newGame('star', 3, 80, 5)"><h3 class="tightSpacing">StarCrossed</h3>Large, fast blobs</a></div>
 
   <div class="col-md-4 col noPadding">
-  <a class="btn btn-black" style="background-color:#196f3d;" onclick="window.throwBalls('wretched', 9, 30, 3)"><h3 class="tightSpacing">Wretched</h3>Small, slow blobs</a></div>
+  <a class="btn btn-black" style="background-color:#196f3d;" onclick="window.newGame('wretched', 9, 30, 2)"><h3 class="tightSpacing">Wretched</h3>Small, slow blobs</a></div>
 </div>
 
 <h2 style="text-align:center;" id="jengaScore"></h2>
@@ -31,6 +28,14 @@ _If you have feedback on tweaking the difficult or settings please let me know!_
 <div id="jenga-div">
     <canvas id="jenga-canvas" resize="true"></canvas>
 </div>
+
+### Explanation
+
+Playing Jenga is mostly an exercise in patience. Especially for the first few pulls, as long as you're willing to check for the loose blocks it's trivial. 
+
+Unlike Jenga there's no way to play this game together; but I still find it a better replacement than raw statistical methods.
+
+If you have suggestions for tweaking the game to make it more suitable, please let me know!
 
 <p><i>Cover Image by <a href="https://pixabay.com/users/thekurupi-3657708/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4027763">Fernando FLeitas</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4027763">Pixabay</a></i></p>
 
--- assets/generator_resources/tensionMinigame.js
+++ assets/generator_resources/tensionMinigame.js
@@ -2,343 +2,350 @@ var firstLayer = project.activeLayer;
 var secondLayer = new Layer();
 secondLayer.activate();
 
-  //CROSSHAIRS
-  targetIn = new Path.Circle(view.center, 25);
-  targetIn.strokeColor = 'white';
-  targetIn.strokeWidth = 6;
+//CROSSHAIRS
+targetIn = new Path.Circle(view.center, 25);
+targetIn.strokeColor = 'white';
+targetIn.strokeWidth = 6;
 
-  targetOut = new Path.Circle(view.center, 25);
-  targetOut.strokeColor = 'black';
-  targetOut.strokeWidth = 3;
+targetOut = new Path.Circle(view.center, 25);
+targetOut.strokeColor = 'black';
+targetOut.strokeWidth = 3;
 
-  cross1 = new Path.Line({
-    from: [view.center.x + 15, view.center.y - 15],
-    to: [view.center.x + 5, view.center.y - 5],
-    strokeColor: 'white',
-    strokeWidth: 4
+cross1 = new Path.Line({
+  from: [view.center.x + 15, view.center.y - 15],
+  to: [view.center.x + 5, view.center.y - 5],
+  strokeColor: 'white',
+  strokeWidth: 4
+});
+
+cross1dark = new Path.Line({
+  from: [view.center.x + 15, view.center.y - 15],
+  to: [view.center.x + 5, view.center.y - 5],
+  strokeColor: 'black',
+  strokeWidth: 2
+});
+
+cross2 = new Path.Line({
+  from: [view.center.x + 15, view.center.y + 15],
+  to: [view.center.x + 5, view.center.y + 5],
+  strokeColor: 'white',
+  strokeWidth: 4
+});
+
+cross2dark = new Path.Line({
+  from: [view.center.x + 15, view.center.y + 15],
+  to: [view.center.x + 5, view.center.y + 5],
+  strokeColor: 'black',
+  strokeWidth: 2
+});
+
+cross3 = new Path.Line({
+  from: [view.center.x - 15, view.center.y - 15],
+  to: [view.center.x - 5, view.center.y - 5],
+  strokeColor: 'white',
+  strokeWidth: 4
+});
+
+cross3dark = new Path.Line({
+  from: [view.center.x - 15, view.center.y - 15],
+  to: [view.center.x - 5, view.center.y - 5],
+  strokeColor: 'black',
+  strokeWidth: 2
+});
+
+cross4 = new Path.Line({
+  from: [view.center.x - 15, view.center.y + 15],
+  to: [view.center.x - 5, view.center.y + 5],
+  strokeColor: 'white',
+  strokeWidth: 4
+});
+
+
+cross4dark = new Path.Line({
+  from: [view.center.x - 15, view.center.y + 15],
+  to: [view.center.x - 5, view.center.y + 5],
+  strokeColor: 'black',
+  strokeWidth: 2
+});
+
+firstLayer.activate();
+
+// kynd.info 2014
+
+function Ball(r, p, v, speed, color) {
+  this.radius = r;
+  this.point = p;
+  this.vector = v;
+  this.maxVec = speed;
+  this.numSegment = Math.floor(r / 3 + 2);
+  this.boundOffset = [];
+  this.boundOffsetBuff = [];
+  this.sidePoints = [];
+  this.path = new Path({
+    fillColor: color
   });
 
-  cross1dark = new Path.Line({
-    from: [view.center.x + 15, view.center.y - 15],
-    to: [view.center.x + 5, view.center.y - 5],
-    strokeColor: 'black',
-    strokeWidth: 2
-  });
+  for (var i = 0; i < this.numSegment; i++) {
+    this.boundOffset.push(this.radius);
+    this.boundOffsetBuff.push(this.radius);
+    this.path.add(new Point());
+    this.sidePoints.push(new Point({
+      angle: 360 / this.numSegment * i,
+      length: 1
+    }));
+  }
+}
 
-  cross2 = new Path.Line({
-    from: [view.center.x + 15, view.center.y + 15],
-    to: [view.center.x + 5, view.center.y + 5],
-    strokeColor: 'white',
-    strokeWidth: 4
-  });
+Ball.prototype = {
+  iterate: function () {
+    this.checkBorders();
+    if (this.vector.length > this.maxVec)
+      this.vector.length = this.maxVec;
+    this.point += this.vector;
+    this.updateShape();
+  },
 
-  cross2dark = new Path.Line({
-    from: [view.center.x + 15, view.center.y + 15],
-    to: [view.center.x + 5, view.center.y + 5],
-    strokeColor: 'black',
-    strokeWidth: 2
-  });
+  checkBorders: function () {
+    var size = view.size;
+    if (this.point.x < -this.radius)
+      this.point.x = size.width + this.radius;
+    if (this.point.x > size.width + this.radius)
+      this.point.x = -this.radius;
+    if (this.point.y < -this.radius)
+      this.point.y = size.height + this.radius;
+    if (this.point.y > size.height + this.radius)
+      this.point.y = -this.radius;
+  },
 
-  cross3 = new Path.Line({
-    from: [view.center.x - 15, view.center.y - 15],
-    to: [view.center.x - 5, view.center.y - 5],
-    strokeColor: 'white',
-    strokeWidth: 4
-  });
+  updateShape: function () {
+    var segments = this.path.segments;
+    for (var i = 0; i < this.numSegment; i++)
+      segments[i].point = this.getSidePoint(i);
 
-  cross3dark = new Path.Line({
-    from: [view.center.x - 15, view.center.y - 15],
-    to: [view.center.x - 5, view.center.y - 5],
-    strokeColor: 'black',
-    strokeWidth: 2
-  });
-
-  cross4 = new Path.Line({
-    from: [view.center.x - 15, view.center.y + 15],
-    to: [view.center.x - 5, view.center.y + 5],
-    strokeColor: 'white',
-    strokeWidth: 4
-  });
-
-
-  cross4dark = new Path.Line({
-    from: [view.center.x - 15, view.center.y + 15],
-    to: [view.center.x - 5, view.center.y + 5],
-    strokeColor: 'black',
-    strokeWidth: 2
-  });
-
-  firstLayer.activate();
-  
-  // kynd.info 2014
-
-  function Ball(r, p, v, speed, color) {
-    this.radius = r;
-    this.point = p;
-    this.vector = v;
-    this.maxVec = speed;
-    this.numSegment = Math.floor(r / 3 + 2);
-    this.boundOffset = [];
-    this.boundOffsetBuff = [];
-    this.sidePoints = [];
-    this.path = new Path({
-      fillColor: color,
-      blendMode: 'lighter'
-    });
-
+    this.path.smooth();
     for (var i = 0; i < this.numSegment; i++) {
-      this.boundOffset.push(this.radius);
-      this.boundOffsetBuff.push(this.radius);
-      this.path.add(new Point());
-      this.sidePoints.push(new Point({
-        angle: 360 / this.numSegment * i,
-        length: 1
-      }));
+      if (this.boundOffset[i] < this.radius / 4)
+        this.boundOffset[i] = this.radius / 4;
+      var next = (i + 1) % this.numSegment;
+      var prev = (i > 0) ? i - 1 : this.numSegment - 1;
+      var offset = this.boundOffset[i];
+      offset += (this.radius - offset) / 15;
+      offset += ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3;
+      this.boundOffsetBuff[i] = this.boundOffset[i] = offset;
     }
+  },
+
+  react: function (b) {
+    var dist = this.point.getDistance(b.point);
+    if (dist < this.radius + b.radius && dist != 0) {
+      var overlap = this.radius + b.radius - dist;
+      var direc = (this.point - b.point).normalize(overlap * 0.15);
+      this.vector += direc;
+      b.vector -= direc;
+
+      this.calcBounds(b);
+      b.calcBounds(this);
+      this.updateBounds();
+      b.updateBounds();
+    }
+  },
+
+  getBoundOffset: function (b) {
+    var diff = this.point - b;
+    var angle = (diff.angle + 180) % 360;
+    return this.boundOffset[Math.floor(angle / 360 * this.boundOffset.length)];
+  },
+
+  calcBounds: function (b) {
+    for (var i = 0; i < this.numSegment; i++) {
+      var tp = this.getSidePoint(i);
+      var bLen = b.getBoundOffset(tp);
+      var td = tp.getDistance(b.point);
+      if (td < bLen) {
+        this.boundOffsetBuff[i] -= (bLen - td) / 2;
+      }
+    }
+  },
+
+  getSidePoint: function (index) {
+    return this.point + this.sidePoints[index] * this.boundOffset[index];
+  },
+
+  updateBounds: function () {
+    for (var i = 0; i < this.numSegment; i++)
+      this.boundOffset[i] = this.boundOffsetBuff[i];
+  },
+
+  removeBall: function () {
+    this.vector = 0;
+    deathFade = this.path.tween({
+      'opacity': 0
+    }, 2000);
+  }
+};
+
+//--------------------- main ---------------------
+
+var gameType = "dread"; //the global game type to remember.
+var balls = [];
+var score = 0;
+
+window.newGame = function (gameName, numBalls, ballSize, ballSpeed){
+  deleteBalls();
+  score = 0;
+  document.getElementById("jengaScore").innerHTML = "Press Enter to Shoot Blobs";
+  gameType = gameName;
+  throwBalls(numBalls, ballSize, ballSpeed);
+}
+
+throwBalls = function (numBalls, ballSize, ballSpeed) {
+  firstLayer.activate();
+
+  if (gameType == "dread") {
+    ballColors = [new Color("#a93226"), new Color("#c0392b"), new Color("#cd6155"), new Color("#ecf0f1"), new Color("#d0d3d4"), new Color("#f2f3f4")];
+  } else if (gameType == "star") {
+    ballColors = [new Color("#d9328b"), new Color("#ffffff"), new Color("#c794c1"), new Color("#eddfec"), new Color("#cfafd2"), new Color("#eddfeb"), new Color("#e6c0d7"), new Color("#f3bf77"), new Color("#6f519c"), new Color("#a27ab2")];
+  } else if (gameType == "wretched") {
+    ballColors = [new Color("#6dbeaf"), new Color("#5faf9e"), new Color("#b1fcfe"), new Color("#a2fcfe"), new Color("#72fafc"), new Color("#82d3be")];
+  } else {
+    ballColors = [new Color(255, 255, 255)];
   }
 
-  Ball.prototype = {
-    iterate: function () {
-      this.checkBorders();
-      if (this.vector.length > this.maxVec)
-        this.vector.length = this.maxVec;
-      this.point += this.vector;
-      this.updateShape();
-    },
+  for (i = 0; i < numBalls; i++) {
+    position = Point.random() * view.size;
+    vector = new Point({
+      angle: 360 * Math.random(),
+      length: Math.random() * 3
+    });
+    radius = Math.random() * 60 + ballSize;
+    balls.push(new Ball(radius, position, vector, ballSpeed, ballColors[Math.floor(Math.random() * ballColors.length)]));
+  }
+}
 
-    checkBorders: function () {
-      var size = view.size;
-      if (this.point.x < -this.radius)
-        this.point.x = size.width + this.radius;
-      if (this.point.x > size.width + this.radius)
-        this.point.x = -this.radius;
-      if (this.point.y < -this.radius)
-        this.point.y = size.height + this.radius;
-      if (this.point.y > size.height + this.radius)
-        this.point.y = -this.radius;
-    },
+deleteBalls = function () {
+  score = 0;
+  while (balls.length > 0) {
+    var deadBall = balls.splice(0, 1); //remove the ball
+    //remove it
+    deadBall[0].removeBall();
+  }
+}
 
-    updateShape: function () {
-      var segments = this.path.segments;
-      for (var i = 0; i < this.numSegment; i++)
-        segments[i].point = this.getSidePoint(i);
+//SPACEBAR
+tool.onKeyDown = function (event) {
+  if (event.key == 'enter') {
+    firstLayer.activate();
+    var blobHit = false;
 
-      this.path.smooth();
-      for (var i = 0; i < this.numSegment; i++) {
-        if (this.boundOffset[i] < this.radius / 4)
-          this.boundOffset[i] = this.radius / 4;
-        var next = (i + 1) % this.numSegment;
-        var prev = (i > 0) ? i - 1 : this.numSegment - 1;
-        var offset = this.boundOffset[i];
-        offset += (this.radius - offset) / 15;
-        offset += ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3;
-        this.boundOffsetBuff[i] = this.boundOffset[i] = offset;
+    for (i = 0; i < balls.length; i++) {
+      if (balls[i].path.contains(view.center)) {
+        var oldBall = balls.splice(i, 1); //remove the ball from list
+        oldBall[0].removeBall();
+        blobHit = true;
       }
-    },
-
-    react: function (b) {
-      var dist = this.point.getDistance(b.point);
-      if (dist < this.radius + b.radius && dist != 0) {
-        var overlap = this.radius + b.radius - dist;
-        var direc = (this.point - b.point).normalize(overlap * 0.015);
-        this.vector += direc;
-        b.vector -= direc;
-
-        this.calcBounds(b);
-        b.calcBounds(this);
-        this.updateBounds();
-        b.updateBounds();
-      }
-    },
-
-    getBoundOffset: function (b) {
-      var diff = this.point - b;
-      var angle = (diff.angle + 180) % 360;
-      return this.boundOffset[Math.floor(angle / 360 * this.boundOffset.length)];
-    },
-
-    calcBounds: function (b) {
-      for (var i = 0; i < this.numSegment; i++) {
-        var tp = this.getSidePoint(i);
-        var bLen = b.getBoundOffset(tp);
-        var td = tp.getDistance(b.point);
-        if (td < bLen) {
-          this.boundOffsetBuff[i] -= (bLen - td) / 2;
-        }
-      }
-    },
-
-    getSidePoint: function (index) {
-      return this.point + this.sidePoints[index] * this.boundOffset[index];
-    },
-
-    updateBounds: function () {
-      for (var i = 0; i < this.numSegment; i++)
-        this.boundOffset[i] = this.boundOffsetBuff[i];
     }
-  };
 
-  //--------------------- main ---------------------
-
-  var gameType = "random"; //the global game type to remember.
-  var score = 0;
-  var balls = [];
-
-  window.throwBalls = function(gameName, numBalls, ballSize, ballSpeed) {
-    document.getElementById('jenga-buttons').style="display:none;";
-    document.getElementById("jengaScore").innerHTML = "Press Enter to Shoot Blobs";
-    gameType = gameName;
-
-    if (gameType == "dread") {
-      ballColors = [new Color("#a93226"), new Color("#c0392b"), new Color("#cd6155"), new Color("#ecf0f1"), new Color("#d0d3d4"), new Color("#f2f3f4")];
-    } else if (gameType == "star") {
-      ballColors = [new Color("#dc2285"), new Color("#8a2b85"), new Color("#613187"), new Color("#d62e35"), new Color("#234459"), new Color("#fce6cc"), new Color("#f9bec6")];
-    } else if (gameType == "wretched") {
-      ballColors = [new Color("#547547"), new Color("#557946"), new Color("#3f6c75"), new Color("#94b9c4"), new Color("#89a982"), new Color("#6b9aa4")];
+    //SCORING STUFF
+    if (blobHit) {
+      score = score + 1;
+      document.getElementById("jengaScore").innerHTML = "Score: " + score;
+      //add two smaller balls
+      window.throwBalls(2, oldBall[0].radius / 2, oldBall[0].maxVec);
     } else {
-      ballColors = [new Color(255, 255, 255)];
-    }
-
-    for (var i = 0; i < numBalls; i++) {
-      position = Point.random() * view.size;
-      vector = new Point({
-        angle: 360 * Math.random(),
-        length: Math.random() * 10
-      });
-      radius = Math.random() * 60 + ballSize;
-      balls.push(new Ball(radius, position, vector, ballSpeed, ballColors[Math.floor(Math.random() * ballColors.length)]));
+      document.getElementById("jengaScore").innerHTML = "You Missed! Start a new game?";
+      deleteBalls();
     }
   }
+}
 
-  window.deleteBalls = function() {
-    score = 0;
-    for (var i = 0; i < balls.length; i++) {
-      alltween = balls[i].path.tween({
-        'opacity': 0
-      }, 1000);
-      alltween.then(function () {
-        balls.splice(i, 1); //remove the ball when it's faded out
-      });
+function onFrame() {
+  for (var i = 0; i < balls.length - 1; i++) {
+    for (var j = i + 1; j < balls.length; j++) {
+      balls[i].react(balls[j]);
     }
   }
-
-  //SPACEBAR
-  tool.onKeyDown = function (event) {
-    if (event.key == 'enter') {
-      blobHit = false;
-      for (var i = 0; i < balls.length - 1; i++) {
-        if (balls[i].path.contains(view.center)) {
-          var oldBall = balls.splice(i, 1); //remove the ball from list
-          // split it into 2 smaller balls
-            window.throwBalls(gameType, 2, oldBall[0].radius / 2, oldBall[0].speed)
-          //fade out old ball
-          oldTween = oldBall[0].path.tween({
-            'opacity': 0
-          }, 2000);
-          blobHit = true;
-        }
-      }
-
-      //SCORING STUFF
-      if (blobHit) {
-        score = score + 1;
-        document.getElementById("jengaScore").innerHTML = "Score: " + score;
-      } else {
-        document.getElementById("jengaScore").innerHTML = "You Missed! Refresh to play again.";
-        deleteBalls();
-      }
-    }
+  for (var i = 0, l = balls.length; i < l; i++) {
+    balls[i].iterate();
   }
+}
 
-  function onFrame() {
-    for (var i = 0; i < balls.length - 1; i++) {
-      for (var j = i + 1; j < balls.length; j++) {
-        balls[i].react(balls[j]);
-      }
-    }
-    for (var i = 0, l = balls.length; i < l; i++) {
-      balls[i].iterate();
-    }
-  }
-
-  function onResize(event) {
-    secondLayer.activate();
+function onResize(event) {
+  secondLayer.activate();
 
   //CROSSHAIRS
   targetIn.remove();
-  targetIn = new Path.Circle(view.center, 25);
+  targetIn = new Path.Circle(view.center, 20);
   targetIn.strokeColor = 'white';
   targetIn.strokeWidth = 6;
 
   targetOut.remove();
-  targetOut = new Path.Circle(view.center, 25);
+  targetOut = new Path.Circle(view.center, 20);
   targetOut.strokeColor = 'black';
   targetOut.strokeWidth = 3;
 
   cross1.remove();
   cross1 = new Path.Line({
-    from: [view.center.x + 15, view.center.y - 15],
-    to: [view.center.x + 5, view.center.y - 5],
+    from: [view.center.x + 13, view.center.y - 13],
+    to: [view.center.x + 2, view.center.y - 2],
     strokeColor: 'white',
     strokeWidth: 4
   });
 
   cross1dark.remove();
   cross1dark = new Path.Line({
-    from: [view.center.x + 15, view.center.y - 15],
-    to: [view.center.x + 5, view.center.y - 5],
+    from: [view.center.x + 13, view.center.y - 13],
+    to: [view.center.x + 2, view.center.y - 2],
     strokeColor: 'black',
     strokeWidth: 2
   });
 
   cross2.remove();
   cross2 = new Path.Line({
-    from: [view.center.x + 15, view.center.y + 15],
-    to: [view.center.x + 5, view.center.y + 5],
+    from: [view.center.x + 13, view.center.y + 13],
+    to: [view.center.x + 2, view.center.y + 2],
     strokeColor: 'white',
     strokeWidth: 4
   });
 
   cross2dark.remove();
   cross2dark = new Path.Line({
-    from: [view.center.x + 15, view.center.y + 15],
-    to: [view.center.x + 5, view.center.y + 5],
+    from: [view.center.x + 13, view.center.y + 13],
+    to: [view.center.x + 2, view.center.y + 2],
     strokeColor: 'black',
     strokeWidth: 2
   });
 
   cross3.remove();
   cross3 = new Path.Line({
-    from: [view.center.x - 15, view.center.y - 15],
-    to: [view.center.x - 5, view.center.y - 5],
+    from: [view.center.x - 13, view.center.y - 13],
+    to: [view.center.x - 2, view.center.y - 2],
     strokeColor: 'white',
     strokeWidth: 4
   });
 
   cross3dark.remove();
   cross3dark = new Path.Line({
-    from: [view.center.x - 15, view.center.y - 15],
-    to: [view.center.x - 5, view.center.y - 5],
+    from: [view.center.x - 13, view.center.y - 13],
+    to: [view.center.x - 2, view.center.y - 2],
     strokeColor: 'black',
     strokeWidth: 2
   });
 
   cross4.remove();
   cross4 = new Path.Line({
-    from: [view.center.x - 15, view.center.y + 15],
-    to: [view.center.x - 5, view.center.y + 5],
+    from: [view.center.x - 13, view.center.y + 13],
+    to: [view.center.x - 2, view.center.y + 2],
     strokeColor: 'white',
     strokeWidth: 4
   });
 
   cross4dark.remove();
   cross4dark = new Path.Line({
-    from: [view.center.x - 15, view.center.y + 15],
-    to: [view.center.x - 5, view.center.y + 5],
+    from: [view.center.x - 13, view.center.y + 13],
+    to: [view.center.x - 2, view.center.y + 2],
     strokeColor: 'black',
     strokeWidth: 2
   });
 
   firstLayer.activate();
-  }
-
+}
